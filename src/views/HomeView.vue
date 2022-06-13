fa506c1fb48b7a20cf680aaa67e1a9582ccad8a3
JulianMGonzalez <monterogjulian@gmail.com>
2022-06-13 17:07:00 -0500

    create home component and store review

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-100 flex justify-center items-center">
+    <StorePreview />
+    <StorePreview />
+    <StorePreview />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StorePreview from "@/components/shop/StorePreview.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    StorePreview,
+  },
+};
 </script>
